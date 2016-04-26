806e70dcefe0e4f41fca7130330b22eb8c3a4c54
KaynEKH <sebastien.corne@outlook.com>
2016-04-26 22:59:29 +0200

    First Class

---
+++ Test/src/Test.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author sebas
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
